57fc10caa25ff5541fceadbc15d04afd60383110
kyungjae park <hankpkj@gmail.com>
2022-07-08 13:28:26 +0900

    solve: aoc 2021 day3

--- _build/default/lib/day3/day3.ml
+++ _build/default/lib/day3/day3.ml
@@ -15,20 +15,41 @@ let rec transpose list =
 
 let transposed = transpose list_of_list
 
-let rec count (zero, one) l =
-  match l with
-  | [] -> if zero < one then '0' else '1'
-  | x :: xs -> count (if x = '0' then (zero + 1, one) else (zero, one + 1)) xs
+let find f l =
+  let rec find_loop (zero, one) l =
+    match l with
+    | [] -> if f zero one then '0' else '1'
+    | x :: xs ->
+        find_loop (if x = '0' then (zero + 1, one) else (zero, one + 1)) xs
+  in
+  find_loop (0, 0) l
 
-let gamma = map (count (0, 0)) transposed
+let bigger = find (fun x y -> x > y)
+let lower = find (fun x y -> x <= y)
+let gamma_char_list = map bigger transposed
 (* gamma 는 [ '0' , '1', '1', '0' , ... ] 과 같은 list of char 인 상태 *)
 
 let reverse_char c =
   match c with '0' -> '1' | '1' -> '0' | _ -> failwith "invalid input"
 
-let epsilon = map reverse_char gamma
-let gamma_string = Base.String.of_char_list gamma
-let epsilon_string = Base.String.of_char_list epsilon
-let gamma_int = int_of_string ("0b" ^ gamma_string)
-let epsilon_int = int_of_string ("0b" ^ epsilon_string)
-let answer1 () = Printf.printf "day2_2 answer is %d\n" (gamma_int * epsilon_int)
+let char_to_int cl = int_of_string ("0b" ^ Base.String.of_char_list cl)
+let gamma = char_to_int gamma_char_list
+let epsilon = char_to_int (map reverse_char gamma_char_list)
+let answer1 () = Printf.printf "day3_1 answer is %d\n" (gamma * epsilon)
+
+let filter f l idx =
+  let most = f (List.nth (transpose l) idx) in
+  (List.filter (fun x -> List.nth x idx = most) l, idx + 1)
+
+let take f =
+  let rec loop (l, num) =
+    match l with
+    | [ last ] -> last
+    | [] -> failwith "invalid input"
+    | xs -> loop (filter f xs num)
+  in
+  loop (list_of_list, 0)
+
+let oxygen = char_to_int (take bigger)
+let co2 = char_to_int (take lower)
+let answer2 () = Printf.printf "day3_2 answer is asdasdas %d\n" (oxygen * co2)
--- _build/default/lib/solution/solution.ml
+++ _build/default/lib/solution/solution.ml
@@ -7,7 +7,8 @@ let print x =
   | 2 ->
       Day2.answer1 ();
       Day2.answer2 ()
-  | 3 -> 
-    Day3.answer1 ();
-    (* Day3.answer2 () *)
+  | 3 ->
+      Day3.answer1 ();
+      Day3.answer2 ();
+      (* Day3.answer3 (); *)
   | _ -> Printf.printf "Haven't solved it yet !"
